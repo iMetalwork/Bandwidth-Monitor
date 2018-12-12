ebc8c1aa527290b3dcd72f4e1247cac09e080c16
Jeremy Turman <turman_jeremy@columbusstate.edu>
2018-12-12 12:22:12 -0500

    Add files via upload

---
+++ WindowsApp2/WindowsApp2/Form1.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblDownload = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblUpload = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(31, 69)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Download"
+        '
+        'lblDownload
+        '
+        Me.lblDownload.AutoSize = True
+        Me.lblDownload.Location = New System.Drawing.Point(170, 69)
+        Me.lblDownload.Name = "lblDownload"
+        Me.lblDownload.Size = New System.Drawing.Size(0, 13)
+        Me.lblDownload.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(34, 108)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Upload"
+        '
+        'lblUpload
+        '
+        Me.lblUpload.AutoSize = True
+        Me.lblUpload.Location = New System.Drawing.Point(173, 108)
+        Me.lblUpload.Name = "lblUpload"
+        Me.lblUpload.Size = New System.Drawing.Size(0, 13)
+        Me.lblUpload.TabIndex = 3
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.lblUpload)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblDownload)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblDownload As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblUpload As Label
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ WindowsApp2/WindowsApp2/Form1.vb
@@ -0,0 +1,114 @@
+﻿
+Imports System.IO
+Imports System.Timers
+
+
+Public Class Form1
+
+    'initialized neccesary variables
+    Dim lblRecieved, lblSent As Long
+    Dim lastRecieved As Long = 0
+    Dim lastSent As Long = 0
+    Dim Loadstr As String = ""
+    Dim SecondsPassed As Integer = 0
+
+    'estabish path for directory
+    Dim dpath As String = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "/Temp"
+
+
+    'establish path for file
+    Dim path As String = dpath & "/Load.txt"
+
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'on Load creates a Directory for files to be saved
+        If Not Directory.Exists(dpath) Then
+            Directory.CreateDirectory(dpath)
+        End If
+
+        'creates file
+        If Not IO.File.Exists(path) Then
+            Dim fs As IO.FileStream = File.Create(path)
+
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+
+        Call GetInfo()
+
+
+        If SecondsPassed > 3 Then
+            lblDownload.Text() = Int(lblRecieved - lastRecieved) / 1000 * 8 & " kb/s"
+
+            lblUpload.Text() = Int(lblSent - lastSent) / 1000 * 8 & " kb/s"
+            SecondsPassed = 0
+        End If
+        SecondsPassed += 1
+        lastRecieved = lblRecieved
+        lastSent = lblSent
+    End Sub
+
+    Public Sub GetInfo()
+        Dim TempLoadstr As String
+        Dim sString() As String
+        Dim i As Integer = 0
+        Dim word As String
+
+        'runs window shell script to get data sent and recieved
+
+
+        Shell("cmd.exe /C netstat -e>" & Chr(34) & path & Chr(34), vbHide, vbTrue)
+
+        'if unable to run shell skips to the end
+        On Error GoTo PassRead
+
+        Dim objReader As IO.StreamReader
+        If IO.File.Exists(path) = True Then
+            objReader = IO.File.OpenText(path)
+
+        Else
+            MsgBox("Yo, the file isn't here!", , "Error")
+
+        End If
+
+
+        'Read the file and store info in loadstr
+        Do While objReader.Peek <> -1
+            i += 1
+            TempLoadstr = objReader.ReadLine()
+
+            If i = 5 Then
+                Loadstr = TempLoadstr
+            End If
+        Loop
+
+        'close the StreamReader
+        objReader.Close()
+
+
+
+
+
+
+        'remove bytes from the string and retain recieved and sent numbers
+        Dim charc As Char() = {" "c}
+        sString = Loadstr.Split(charc, StringSplitOptions.RemoveEmptyEntries)
+
+        'convert received and sent nummbers to ints based on location in string array
+        lblRecieved = Int(sString(1))
+        lblSent = Int(sString(2))
+
+PassRead:
+
+    End Sub
+
+
+
+
+
+End Class
